6c702c3799a74d7653a7b2992e427311f23e2b59
Justin Ales <justin.ales@gmail.com>
2018-03-15 12:44:40 +0000

    Added functions to determine required savings amount in dollars and as percentage of projected income to hit total savings target

--- R/functions.R
+++ R/functions.R
@@ -101,6 +101,141 @@ income_projection <- function(start, increase, increases_per_year=1, years, uppe
 	c(start, pmin(start * (1 + increase)^(1:(years-1)), upper_limit))
 }
 
+#' Savings calculator
+#' 
+#' Given a total savings target calculalate how much per 
+#' year needed to achieve target savings total
+#'  
+#' @param amount Total savings target
+#' @param years Number of years to calculate
+#' @param prudence Parameter for \code{investment_returns}, 50 or 65
+#' @param fund Parameter for \code{investment_returns}, "USS", "Growth fund", "Moderate growth fund", "Cautious growth fund", or "Cash fund"
+#' @export
+#' @return Dollars needed to save per year. 
+savings_required_constant <- function(amount,years,prudence,fund)
+{
+  
+#savings <- investment_savings_total(10000,years,prudence,fund)
+ #
+requiredSavingsPerYear = optimize(constantCostFunction,c(0, 2*amount/years),amount,years,prudence,fund)
+
+return(as.numeric(requiredSavingsPerYear[1]))
+}
+
+
+#' Savings calculator
+#' 
+#' Given a total savings target calculalate how much per 
+#' year needed to achieve target savings total
+#'  
+#' @param amount Total savings target
+#' @param income Projected income each year
+#' @param prudence Parameter for \code{investment_returns}, 50 or 65
+#' @param fund Parameter for \code{investment_returns}, "USS", "Growth fund", "Moderate growth fund", "Cautious growth fund", or "Cash fund"
+#' @export
+#' @return Dollars needed to save per year. 
+savings_required_percentage <- function(amount,income,prudence,fund)
+{
+  
+  #savings <- investment_savings_total(10000,years,prudence,fund)
+  #
+  requiredSavingsPerYear = optimize(percentageCostFunction,c(0, 1),amount,income,prudence,fund)
+  
+  return(as.numeric(requiredSavingsPerYear[1]))
+}
+#' Savings calculator
+#' 
+#' Given constant amount saved per year calculate total saved at end of years 
+#'  
+#' @param savedPerYear Constant amount saved per year
+#' @param years Number of years to calculate
+#' @param prudence Parameter for \code{investment_returns}, 50 or 65
+#' @param fund Parameter for \code{investment_returns}, "USS", "Growth fund", "Moderate growth fund", "Cautious growth fund", or "Cash fund"
+#' @export
+#' @return Dollars needed to save per year. 
+investment_savings_total <- function(savedPerYear,years,prudence,fund)
+{
+  
+  ret <- subset(investment_returns(), Prudence==prudence & Fund==fund)$growth
+  
+
+  totalSavings = numeric(years)
+  totalSavings[1] <- savedPerYear
+  for(i in 2:years)
+  {
+    totalSavings[i] <- totalSavings[i-1] * (1 + ret[i]) + savedPerYear
+  }
+  
+  return(totalSavings[years])
+}
+
+#' Savings calculator
+#' 
+#' Given percent of salary saved per year calculate total saved at end of years 
+#'  
+#' @param savedPerYear Percentage saved per year
+#' @param income vector of projected income values for each year
+#' @param prudence Parameter for \code{investment_returns}, 50 or 65
+#' @param fund Parameter for \code{investment_returns}, "USS", "Growth fund", "Moderate growth fund", "Cautious growth fund", or "Cash fund"
+#' @export
+#' @return Dollars needed to save per year. 
+percentage_savings_total <- function(savedPerYear,income,prudence,fund)
+{
+  
+  ret <- subset(investment_returns(), Prudence==prudence & Fund==fund)$growth
+  
+  
+  totalSavings = numeric(length(income))
+  totalSavings[1] <- savedPerYear*income[1]
+  
+  for(i in 2:length(income))
+  {
+    totalSavings[i] <- totalSavings[i-1] * (1 + ret[i]) + savedPerYear*income[i]
+  }
+  
+  return(totalSavings[length(income)])
+}
+#' Cost function for constant amount
+#' 
+#' This function is the cost function used for determining how much is needed 
+#' to be saved as a constant amount per year. 
+#'  
+#' @param savedPerYear Total savings target
+#' @param years Number of years to calculate
+#' @param prudence Parameter for \code{investment_returns}, 50 or 65
+#' @param fund Parameter for \code{investment_returns}, "USS", "Growth fund", "Moderate growth fund", "Cautious growth fund", or "Cash fund"
+#' @export
+#' @return Dollars needed to save per year. 
+constantCostFunction <- function(savedPerYear,targetTotal,years,prudence,fund)
+{
+  totalSavings = investment_savings_total(savedPerYear,years,prudence,fund)
+  
+  targetError = sum((totalSavings-targetTotal)^2)
+  
+  return(targetError)
+}
+
+
+#' Cost function for percetage saved
+#' 
+#' This function is the cost function used for determining how much is needed 
+#' to be saved as a percentage of income each year. 
+#'  
+#' @param savedPerYear Percentage saved each year
+#' @param targetTotal  Target total savings amount
+#' @param income Income earned in each year
+#' @param prudence Parameter for \code{investment_returns}, 50 or 65
+#' @param fund Parameter for \code{investment_returns}, "USS", "Growth fund", "Moderate growth fund", "Cautious growth fund", or "Cash fund"
+#' @export
+#' @return Dollars needed to save per year. 
+percentageCostFunction <- function(savedPerYear,targetTotal,income,prudence,fund)
+{
+  totalSavings = percentage_savings_total(savedPerYear,income,prudence,fund)
+  
+  targetError = sum((totalSavings-targetTotal)^2)
+  
+  return(targetError)
+}
 #' Calculate annuity rates
 #' 
 #' Different rates for sex, joint vs single, and expected annual life expectancy increase
