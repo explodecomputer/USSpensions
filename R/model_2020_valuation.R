1469883873b352a7ef83d00ce71bf65875eba3d6
Gibran Hemani <explodecomputer@gmail.com>
2021-04-21 00:07:18 +0100

    updated model

---
+++ R/model_2020_valuation.R
@@ -0,0 +1,201 @@
+#' Investment returns for the 2020 valuation
+#'
+#' @export
+#' @return Data frame
+investment_returns_2020 <- function(years)
+{
+	rbind(
+		dplyr::tibble(
+			Scenario="Scenario 1",
+			Year=2022:2070,
+			growth=0
+		),
+		dplyr::tibble(
+			Scenario="Scenario 2a",
+			Year=2022:2070,
+			growth=0.001
+		),
+		dplyr::tibble(
+			Scenario="Scenario 2b",
+			Year=2022:2070,
+			growth=0.001
+		),
+		dplyr::tibble(
+			Scenario="Scenario 3a",
+			Year=2022:2070,
+			growth=0.002
+		),
+		dplyr::tibble(
+			Scenario="Scenario 3b",
+			Year=2022:2070,
+			growth=0.002
+		)
+	) %>%
+		filter(Year %in% years)
+}
+
+
+
+#' Calculate annuity rates for 2020 valuation
+#' 
+#' Different rates for sex, joint vs single, and expected annual life expectancy increase
+#' 
+#' @param sex "men" or "women"
+#' @param type "single" or "joint"
+#' @param years number of years to calculate
+#' @param le_increase Expected increase in life expectancy per year. Default 0.015
+#' 
+#' @export
+#' @return vector of annuities
+annuity_rates <- function(sex, type, years, le_increase=0.005)
+{
+	annuities_quote <- tribble(
+		~age, ~Type, ~Sex, ~val,
+		66, "single", "men", 2875.00,
+		67, "single", "men", 3028.00,
+		68, "single", "men", 3179.00,
+		66, "single", "women", 2875.00,
+		67, "single", "women", 3028.00,
+		68, "single", "women", 3179.00,
+		66, "joint", "men", 2469.00, 
+		67, "joint", "men", 2588.00, 
+		68, "joint", "men", 2733.00, 
+		66, "joint", "women", 2720.00,
+		67, "joint", "women", 2877.00,
+		68, "joint", "women", 3045.00
+	)
+
+	annuity <- rep(0, years)
+	annuity[1] <- subset(annuities_quote, Sex==sex & Type==type & age==66)$val
+	annuity[2] <- annuity[1] / (1+le_increase)
+	annuity[3] <- subset(annuities_quote, Sex==sex & Type==type & age==66)$val / (1+le_increase)^2
+	for(i in 4:6)
+	{
+		annuity[i] <- annuity[i-1] / (1 + le_increase)
+	}
+	annuity[7] <- subset(annuities_quote, Sex==sex & Type==type & age==67)$val / (1+le_increase)^6
+	annuity[8] <- annuity[7] / (1+le_increase)
+	annuity[9] <- annuity[8] / (1+le_increase)
+	annuity[10] <- subset(annuities_quote, Sex==sex & Type==type & age==67)$val / (1+le_increase)^9
+	for(i in 10:24)
+	{
+		annuity[i] <- annuity[i-1] / (1 + le_increase)
+	}
+	annuity[25] <- subset(annuities_quote, Sex==sex & Type==type & age==68)$val / (1+le_increase)^27
+	for(i in 26:49)
+	{
+		annuity[i] <- annuity[i-1] / (1 + le_increase)
+	}
+	return(annuity[1:years])
+}
+
+
+
+#' Pension scenarios 2020
+#'
+#'
+#' @param income Output from income_projection()
+#' @param annuity Output from annuity_rates()
+#' @param scenario Investment scenario for the DC component of the current scheme ("Scenario 1", "Scenario 2a", "Scenario 2b", "Scenario 3a", "Scenario 3b")
+#'
+#' @export
+#' @return List of data frames for each scenario
+pension_calculation_2020 <- function(income, annuity, scenario)
+{
+	years <- 1:length(income) + today() %>% year()
+
+	out <- list()
+	out$scenario_1 <- calc_db_dc(
+		subset(investment_returns_2020(years), Scenario=="Scenario 1")$growth,
+		annuity,
+		income,
+		prop_salary=0,
+		db_cutoff = 0,
+		employee_cont = 0.096,
+		employer_cont = 0.018,
+		incr = 1,
+		mult = 3
+	)
+
+	out$scenario_2a <- calc_db_dc(
+		subset(investment_returns_2020(years), Scenario=="Scenario 2a")$growth,
+		annuity,
+		income,
+		prop_salary = 1/170,
+		employee_cont = 0.12,
+		employer_cont = 0,
+		db_cutoff = 40000,
+		incr = 1,
+		mult = 3
+	)
+
+	out$scenario_2b <- calc_db_dc(
+		subset(investment_returns_2020(years), Scenario=="Scenario 2b")$growth,
+		annuity,
+		income,
+		prop_salary = 1/165,
+		employee_cont = 0.12,
+		employer_cont = 0,
+		db_cutoff = 30000,
+		incr = 1,
+		mult = 3
+	)
+
+	out$scenario_3a <- calc_db_dc(
+		subset(investment_returns_2020(years), Scenario=="Scenario 3a")$growth,
+		annuity,
+		income,
+		prop_salary = 1/115,
+		employee_cont = 0.16,
+		employer_cont = 0,
+		db_cutoff = 40000,
+		incr = 1,
+		mult = 3
+	)
+
+	out$scenario_3b <- calc_db_dc(
+		subset(investment_returns_2020(years), Scenario=="Scenario 3b")$growth,
+		annuity,
+		income,
+		prop_salary = 1/110,
+		employee_cont = 0.16,
+		employer_cont = 0,
+		db_cutoff = 30000,
+		incr = 1,
+		mult = 3
+	)
+
+	out$current <- calc_db_dc(
+		subset(investment_returns_2020(years), Scenario==scenario)$growth,
+		annuity,
+		income,
+		employee_cont = 0.08,
+		employer_cont = 0.12,
+		prop_salary = 1/75,
+		db_cutoff =  59883.65,
+		incr = 1,
+		mult = 3
+	)
+
+	##########
+
+	return(out)
+}
+
+
+#' Summarise the output from pension_calculation_2020()
+#'
+#' @param pension Output from pension_calculation_2020()
+#'
+#' @export
+#' @return data frame
+pension_calculation_2020_summary <- function(pension)
+{
+	lapply(names(pension), function(x)
+	{
+		pension[[x]] %>% 
+			dplyr::slice_tail(n=1) %>%
+			dplyr::select(total_pot, total_pension) %>%
+			dplyr::mutate(scenario=x)
+	}) %>% bind_rows()
+}
